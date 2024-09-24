a54f9f424aeffc5a5a152c153aeeb4d4105cf4d0
leo <lizhi1@fosun.com>
2024-09-24 14:55:42 +0800

    feat: 调整打包模式改成静态构建

--- src/app/components/screen_footer/index.tsx
+++ src/app/components/screen_footer/index.tsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div className="w-screen h-screen bg-black">Footer</div>;
+  return (
+    <div className="w-screen h-screen bg-black">
+      版权所有 © 2024 憬象智能保留所有权利。
+    </div>
+  );
 };
 
 export default Footer;
