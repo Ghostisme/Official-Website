4d25178670e96356058ea780a6a9a5dad115caa2
leo <leo312137924@163.com>
2024-10-20 18:27:55 +0800

    feat: 变更访问地址

--- src/app/hooks/jumpPage.ts
+++ src/app/hooks/jumpPage.ts
@@ -2,7 +2,7 @@ import { useState } from "react"
 const useJumpPage = () => {
     const [] = useState()
     const handleClick = () => {
-        window.open("http://47.116.163.237:8190/", "_blank")
+        window.open("http://116.62.226.8:8190/", "_blank")
     }
     return {
         handleClick
